7d8e3bd754bcc4d8f139c9edca3b734f3545d126
318236213 <zain.muhammad@rbc.com>
2016-04-07 16:05:54 -0400

    added more stuff

---
+++ app/src/main/java/com/rbc/rbcbudgets/CategoryPickerDialog.java
@@ -0,0 +1,126 @@
+package com.rbc.rbcbudgets;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+/**
+ * Created by platohe on 2016-04-07.
+ */
+public class CategoryPickerDialog extends android.support.v4.app.DialogFragment implements AdapterView.OnItemSelectedListener {
+    private String category_array[];
+    private String food_array[];
+    private String clothing_array[];
+    private String electronics_array[];
+    private String entertainment_array[];
+
+    private CategoryAddListener callBack;
+    private Spinner categorySpinner, itemSpinner;
+    private EditText input;
+    private String item = "Fast Food";
+    private int categoryIndex = 0;
+
+    public interface CategoryAddListener{
+        void addItem(int category, String item);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_category_picker, null);
+
+        categorySpinner = (Spinner) v.findViewById(R.id.category_spinner);
+        itemSpinner = (Spinner) v.findViewById(R.id.item_spinner);
+        input = (EditText) v.findViewById(R.id.item_input);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.step_two_categories, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.food_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemSpinner.setAdapter(adapter2);
+
+        categorySpinner.setOnItemSelectedListener(this);
+        itemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (categoryIndex){
+                    case 0:
+                        item = food_array[position];
+                        break;
+                    case 1:
+                        item = clothing_array[position];
+                        break;
+                    case 2:
+                        item = electronics_array[position];
+                        break;
+                    case 3:
+                        item = entertainment_array[position];
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        builder.setView(v);
+        builder.setTitle("Choose an Item");
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callBack.addItem(categoryIndex, item);
+            }
+        });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // check for other.
+        if(position == category_array.length - 1){
+            itemSpinner.setVisibility(View.GONE);
+            input.setVisibility(View.VISIBLE);
+        }  else {
+            itemSpinner.setVisibility(View.VISIBLE);
+            input.setVisibility(View.GONE);
+        }
+
+        categoryIndex = position;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        category_array = getActivity().getResources().getStringArray(R.array.step_two_categories);
+        food_array = getActivity().getResources().getStringArray(R.array.food_array);
+        clothing_array = getActivity().getResources().getStringArray(R.array.clothing_array);
+        electronics_array = getActivity().getResources().getStringArray(R.array.electronics_array);
+        entertainment_array = getActivity().getResources().getStringArray(R.array.entertainment_array);
+    }
+
+    public void setListener(CategoryAddListener callBack){
+        this.callBack = callBack;
+    }
+}
--- app/src/main/java/com/rbc/rbcbudgets/CreateBudgetStepTwoFragment.java
+++ app/src/main/java/com/rbc/rbcbudgets/CreateBudgetStepTwoFragment.java
@@ -1,21 +1,27 @@
 package com.rbc.rbcbudgets;
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateBudgetStepTwoFragment extends FormFragment {
+public class CreateBudgetStepTwoFragment extends FormFragment implements CategoryPickerDialog.CategoryAddListener, View.OnClickListener {
     private FormCompletion mCallBack;
+    private Button add;
 
     public CreateBudgetStepTwoFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void addItem(int category, String item) {
+        Log.i("category = " + category + " item = " + item, "");
     }
 
     private void checkForm(boolean inputsFilled){
@@ -41,7 +47,19 @@ public class CreateBudgetStepTwoFragment extends FormFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_budget_step_two, container, false);
+        View v = inflater.inflate(R.layout.fragment_create_budget_step_two, container, false);
+
+        add = (Button) v.findViewById(R.id.step_two_add);
+        add.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        CategoryPickerDialog dialog = new CategoryPickerDialog();
+        dialog.setListener(this);
+        dialog.show(getActivity().getSupportFragmentManager(), "Category Picker");
     }
 
     @Override
